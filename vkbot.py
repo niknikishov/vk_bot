7aebf7ad3c0a8f8ada4bc3ece78ab1cab1e0c104
niknikishov <kol_93@mail.ru>
2022-03-01 20:31:56 +0400

    comment code

--- vkbot.py
+++ vkbot.py
@@ -1,5 +1,6 @@
 from pprint import pprint
-
+# TODO добавить в бота функционал обработки и ретуши фотографий, подборку песен из группы,
+#  выборку случайных новостей стены
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from data import token, group_id
@@ -15,28 +16,28 @@ class VkBot:
         pass
 
     def run(self):
-            vk_session = vk_api.VkApi(token=self.token)
-            longpoll = VkBotLongPoll(vk_session, self.group_id)
+            vk_session = vk_api.VkApi(token=self.token)  # cессия с api через токен
+            longpoll = VkBotLongPoll(vk_session, self.group_id)  # класс бота, прикреплен к сообществу ВК
             vk = vk_session.get_api()
 
-            for event in longpoll.listen():
+            for event in longpoll.listen():  # функция прослушивания сервера, реагирует на события
                 # pprint(event)
                 try:
-                    if event.type == VkBotEventType.MESSAGE_NEW:
+                    if event.type == VkBotEventType.MESSAGE_NEW:  # обработка нового сообщения
                         print(f"Новое сообщение от {event.object['message']['from_id']}!!!")
                         print(event.object['message']['text'])
                         # print(event)
-
+# TODO прописать условие приветственного сообщения, необходима реакция только на первое сообщение
                         vk.messages.send(
-                            user_id=event.object['message']['from_id'],
+                            user_id=event.object['message']['from_id'],  # отправка приветственного сообщения
                             random_id=get_random_id(),
                             message='Hello'
                         )
-                    elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
+                    elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:  # обработка статуса набора сообщения
                         print(f"{event.object['from_id']} печатает... для {event.object['to_id']}")
 
                     elif event.type == VkBotEventType.MESSAGE_REPLY:  # возникает в случае отправки сообщения ботом
-                        print('Новое сообщение:')               # событие типа ответ
+                        print('Новое сообщение:')               # событие типа ответ, пишет статус обработки в консоль
 
                         print('От меня для: ', end='')
 
